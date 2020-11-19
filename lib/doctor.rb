c4a7c7eb356576408ec1228c7b576f1a2471c22f
Sarah Thiery <sarahvthiery@gmail.com>
2020-11-19 18:44:25 -0500

    commit tree 7472c5baf6491c2bc105e966512895513445b60e

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,33 @@
+class Doctor
+    attr_accessor :name
 
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        all_patients = []
+        Appointment.all.select do |appointment|
+            if appointment.doctor == self
+                all_patients << appointment.patient
+            end
+        end
+        all_patients.uniq
+    end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,26 @@
+class Genre
+    attr_accessor :name, :song, :artist
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        all_artists = []
+        Song.all.select do |song| 
+            if song.genre == self
+                all_artists << song.artist
+            end
+        end
+        all_artists.uniq
+    end
+end
