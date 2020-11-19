332e11b02ef304824bd4a17af616ecc643864e62
Sarah Thiery <sarahvthiery@gmail.com>
2020-11-19 18:45:58 -0500

    created classes with has many through relationships

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,34 @@
+class Patient
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            if appointment.patient == self
+                appointment.date
+            end
+        end
+    end
+
+    def doctors
+        all_doctors = []
+        Appointment.all.select do |appointment|
+            if appointment.patient == self
+                all_doctors << appointment.doctor
+            end
+        end
+        all_doctors.uniq
+    end
+end
